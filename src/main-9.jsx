a043332e59307dbb4d640ec8a590d55a6962495a
dktwr4700-lgtm <dktwr4700@gmail.com>
2026-08-16 18:36:31 +0400

    Add files via upload

---
+++ src/main-9.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Register from "./Register.jsx";
+import Login from "./Login.jsx";
+
+function Root() {
+  const [hash, setHash] = React.useState(window.location.hash.replace("#", ""));
+
+  React.useEffect(() => {
+    const onHashChange = () => setHash(window.location.hash.replace("#", ""));
+    window.addEventListener("hashchange", onHashChange);
+    return () => window.removeEventListener("hashchange", onHashChange);
+  }, []);
+
+  if (hash === "register") return <Register />;
+  if (hash === "login") return <Login />;
+  return <App />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
