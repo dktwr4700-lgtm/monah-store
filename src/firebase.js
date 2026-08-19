0ea9bb5e0721d5c66db404c4e36007e21222ee21
dktwr4700-lgtm <dktwr4700@gmail.com>
2026-08-20 03:40:39 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -17,3 +17,21 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+// يضمن وجود جلسة دخول (حتى لو مجهولة) قبل أي عملية تحتاج صلاحية
+// يُستخدم في صفحة المنتج قبل إصدار تصريح تحميل الملف
+export function ensureAnonymousAuth() {
+  return new Promise((resolve, reject) => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        unsub();
+        resolve(user.uid);
+      } else {
+        signInAnonymously(auth).catch((err) => {
+          unsub();
+          reject(err);
+        });
+      }
+    });
+  });
+}
